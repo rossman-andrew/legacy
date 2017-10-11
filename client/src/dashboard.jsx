d012c0702cae062f833a81b5e108b95e691dba94
jackmqiu <jackqiu2016@gmail.com>
2017-10-11 10:13:40 -0700

    Chatbox changes before rebase

--- client/src/dashboard.jsx
+++ client/src/dashboard.jsx
@@ -20,8 +20,12 @@ import TripDashboard from './components/tripDashboard/tripDashboard.jsx';
 import MapboxViewer from './components/mapboxViewer.jsx';
 import ExpenseTracker from './components/expenseTracker/expenseTracker.jsx';
 import Landmarks from './components/landmarks/landmarks.jsx';
+<<<<<<< HEAD
 import navData from './components/tripDashboard/dummyData.js';
 import TripNavBar from './components/tripDashboard/tripNavBar.jsx';
+=======
+import Chatbox from './components/Chatbox/index.jsx';
+>>>>>>> Chatbox changes before rebase
 
 const SERVER_URL = HOSTNAME;
 
@@ -103,10 +107,38 @@ class Dashboard extends React.Component {
 	render() {
 		return(
 			<div>
+<<<<<<< HEAD
 				<h3>Hello {store.getState().user.name}, welcome back</h3>
 				<Button onClick={this.handleLogout}>Logout</Button>
 				{this.showNavBar()}
 				{this.getViewComponent()}
+=======
+				<div className="navbar">
+          <ul>
+            <li id="title">The Travel App</li>
+            <li className="link">Home</li>
+            <li className="link">News</li>
+            <li className="link">Contact</li>
+          </ul>
+        </div>
+
+        <div className="dashbody">
+
+        	<Row className="manager-main">
+        		<Col md={7} mdOffset={2}>
+							<h3>Hello {store.getState().user.name}, welcome back</h3>
+						</Col>
+
+						<Col md={2}>
+							<Button id="logoutbutton" onClick={this.handleLogout}>Logout</Button>
+						</Col>
+					</Row>
+
+					<button id="hide" onClick={() => store.dispatch(reducer.changeView('TripManager'))}>Trip Manager</button>
+					{this.getViewComponent()}
+				</div>
+				<Chatbox/>
+>>>>>>> Chatbox changes before rebase
 			</div>
 		)
 	}
