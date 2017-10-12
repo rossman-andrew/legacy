75e680bf2acaa34068bd48c076011de3994fe1f0
Enrique Hernandez <enriquehernndez@Enriques-MacBook-Pro.local>
2017-10-12 15:07:11 -0700

    ready to merge with tripPopup implemented

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
+/**
+ * These rules enforce the Hack Reactor Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/reactorcore/eslint-config-hackreactor
+ */
+
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: './node_modules/eslint-config-hackreactor/index.js'
 };
