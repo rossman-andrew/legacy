f7b409590eb78bea5a6cf301bf4b46ce2562ec3f
Andrew Rossman <andrew100r@gmail.com>
2017-10-12 12:31:01 -0700

    (feat) Implements api call to google images for trip gallery

--- client/src/components/tripDashboard/TripGallery.jsx
+++ client/src/components/tripDashboard/TripGallery.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import $ from 'jquery';
 import { Image, Grid } from 'semantic-ui-react';
 
 class TripGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      tripPics: [],
       featuredImage: ''
     };
   }
 
   componentDidMount() {
+    $.ajax({
+      url: `https://www.googleapis.com/customsearch/v1?key=AIzaSyBEkRzfpS6T7dZcLaYA9lQdzMJNDSrgOgg&cx=012965794133406592343:m7o6dpksdcc&q=${'image of ' + this.props.trip.location}&searchType=image`, 
+      success: (data) => { 
+        console.log(data); 
+      },
+      error: (err) => { 
+        console.log('error'); 
+      }  
+    });
     this.setState({
       featuredImage: 'http://www.telegraph.co.uk/content/dam/Travel/2017/July/fira-santorini-greece-sun-xlarge.jpg'
     });
