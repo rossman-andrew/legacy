effcf2d3076ab3b3c734db862fc79dd94967c47f
Enrique Hernandez <enriquehernndez@Enriques-MacBook-Pro.local>
2017-10-12 17:29:38 -0700

    navBar styled

--- client/src/components/tripDashboard/tripNavBar.jsx
+++ client/src/components/tripDashboard/tripNavBar.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import reducer from '../../Reducers';
 import { Button } from 'react-bootstrap';
 import { ButtonGroup } from 'react-bootstrap';
+import { Menu } from 'semantic-ui-react';
 
-const TripNavBar = (props) => {
-  return (
-    <ButtonGroup className="tripnav">
-      <Button className="btn" onClick={props.logout}>Logout</Button>
-      {props.features.map((feature, index) => {
-        return <Button key={index} className="btn" onClick={() => {
-          props.dispatch(reducer.changeView(feature.link));
-        }}>{feature.name}</Button>;
-      })}
-    </ButtonGroup>
-  );
+class TripNavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: null
+    };
+
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick(e, { name }) {
+    this.setState({activeItem: name});
+  }
+
+  render() {
+    return (
+      <Menu>
+        <Menu.Item className="btn" onClick={this.props.logout}>Logout
+        </Menu.Item>
+
+        {this.props.features.map((feature, index) => {
+          return <Menu.Item key={index} className="btn" onClick={() => {
+            this.props.dispatch(reducer.changeView(feature.link));
+          }}>{feature.name}</Menu.Item>;
+        })}
+
+      </Menu>
+    );
+  }
 };
 
 export default TripNavBar;
