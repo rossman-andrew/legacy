ab129ad17f61ef0486be296a6b2fcc6cd11b4af5
jackmqiu <jackqiu2016@gmail.com>
2017-10-13 19:57:47 -0700

    messages don't have titles if they 're from the same person

--- client/src/components/Chatbox/index.jsx
+++ client/src/components/Chatbox/index.jsx
@@ -10,9 +10,13 @@ class Chatbox extends Component {
     super(props)
     this.handleToggle = this.handleToggle.bind(this);
     this.state = {
-      messages: [['Hi!', 'Jack']],
+      messages: [['Hi! Please answer some questions to help me better find the perfect trip for you.', 'Jack', 0]],
+      answers: [],
       term: '',
-      replyNumber: 1
+      replyNumber: 1,
+      answerNumber: 0,
+      ChatboxInitialized: false,
+      LastMessageUser: 'Jack'
     }
     this.onInputChange = this.onInputChange.bind(this)
     this.onFormSubmit = this.onFormSubmit.bind(this)
@@ -28,8 +32,17 @@ class Chatbox extends Component {
     event.preventDefault();
     console.log('called', event);
     //handle searching web data
+    var counter;
+    if(this.state.LastMessageUser !== 'props.user'){
+      counter = 0;
+    } else {
+      counter = 1;
+    }
     this.setState((prevState, props) => {
-      return {messages: prevState.messages.concat([[prevState.term, 'props.user']])};
+      return {
+        messages: prevState.messages.concat([[prevState.term, 'props.user', counter]]),
+        LastMessageUser: 'props.user'
+      };
     })
     this.setState({term:''});
     this.getReply();
@@ -37,6 +50,26 @@ class Chatbox extends Component {
   }
 
   getReply() {
+    setTimeout(
+      () => {
+        this.setState((prevState, props) => {
+          return {
+            LastMessageUser: 'Jack',
+            messages: prevState.messages.concat([prevState.answers[prevState.answerNumber]]),
+            answerNumber: prevState.answerNumber + 1
+          }
+        })
+        this.getQuestion();
+      }, 2000
+    )
+  }
+
+  getQuestion() {
+    this.setState((prevState, props) => {//makes it so only on first Chatbox Toggle do you get new question
+      return {
+        ChatboxInitialized: true
+      }
+    })
     axios({
       method: 'post',
       url: '/getGuideReplies',
@@ -46,14 +79,26 @@ class Chatbox extends Component {
     })
     .then((reply) => {
       console.log(reply);
+      var counter;
+      if(this.state.LastMessageUser === 'props.user'){
+        counter = 0;
+        this.setState((prevState, props) => {
+          return {LastMessageUser: 'Jack'}
+        })
+      } else {
+        counter = 1;
+      }
+      setTimeout(
+        () => {this.setState((prevState, props) => {
+          return {
+            messages: prevState.messages.concat([[reply.data[0].question, 'Jack', counter]]),
+            answers: prevState.answers.concat([[reply.data[0].reply, 'Jack', 0]])
+          }
+        })}, 3000
+      )
       this.setState((prevState, props) => {
         return {replyNumber: prevState.replyNumber + 1};
       })
-      setTimeout(
-        () => {this.setState((prevState, props) => {
-          return {messages: prevState.messages.concat([[reply.data[0].question, 'Jack']])}
-        })}, 2000
-      )
     })
     .catch(function (error) {
       console.log(error);
@@ -63,7 +108,9 @@ class Chatbox extends Component {
   handleToggle(toggled) {
     console.log('handle toggle', toggled);
     this.props.dispatch(reducer.toggleChatbox(toggled));
-    this.getReply();
+    if(!this.state.ChatboxInitialized){
+      this.getQuestion();
+    }
   }
 
 
@@ -71,7 +118,7 @@ class Chatbox extends Component {
     console.log('this.props', this.props);
     if(this.props.toggled){
       return (
-        <div className="ui card">
+        <div className="ui bottom fixed card">
           <div className="ui feed">
             {
               this.state.messages.map((message) => {
@@ -79,7 +126,13 @@ class Chatbox extends Component {
                   <div className="event">
                     <div className="content">
                       <div className="summary">
-                        <a className="user">{message[1]}</a>
+                        {
+                          (() => {
+                            if(message[2] < 1) {
+                              return <a className="user">{message[1]}</a>
+                            }
+                          })()
+                        }
                         <div className="extra text">{message[0]}</div>
                       </div>
                     </div>
@@ -90,7 +143,7 @@ class Chatbox extends Component {
           </div>
           <form onSubmit={this.onFormSubmit}>
             <div className="ui action input focus" >
-              <input type="text" placeholder="Chat.." onChange = {this.onInputChange}></input>
+              <input type="text" placeholder="Chat.." value={this.state.term} onChange = {this.onInputChange}></input>
               <button className="ui button" type="submit">Send</button>
             </div>
           </form>
@@ -99,7 +152,7 @@ class Chatbox extends Component {
       )
     } else {
       return(
-        <button className="ui secondary button" onClick={() => {this.handleToggle(true)} }> Untoggled Box! </button>
+        <button className="ui bottom fixed secondary button" onClick={() => {this.handleToggle(true)} }> Untoggled Box! </button>
       )
     }
   }
